84325151a2a9ade99faae084ce24f67047dca838
hdp2112 <hdp2112@columbia.edu>
2019-12-10 17:50:47 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function test() {
+  let swagform = document.querySelector('#create-task-form');
+  let input = document.querySelector('#new-task-description');
+  let ul = document.querySelector('#tasks');
+  
+  swagform.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let li = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = 'Delete';
+    
+    let p = document.createElement('p');
+    p.innerText = `${input.value}`
+    li.append(p);
+
+    button.addEventListener('click', function (event) {
+      let parentLi = event.target.parentNode;
+      parentLi.remove();
+    });
+    li.append(button);
+
+    ul.appendChild(li);
+  });
 });
