46ad98798f037a978fe2662a5c91b85cdba865af
Pavel Beletsky <pavel.beletsky@live.com>
2019-12-12 01:06:31 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+  const form = document.querySelector("#create-task-form")
+  const input = document.querySelector('#new-task-description')
+  const prLabel = document.createElement('label')
+  prLabel.innerText = ' Priority: '
+  input.insertAdjacentElement('afterend', prLabel)
+  const prSelect = document.createElement('select')
+  prSelect.innerHTML = '<option disabled selected>priority</option> <option>High</option> <option>Medium</option> <option>Low</option>'
+  prLabel.insertAdjacentElement('afterend', prSelect)
+
+
+  form.addEventListener("submit", function(e){
+    e.preventDefault()
+
+    const ul = document.querySelector("ul")
+    let li = document.createElement("li")
+    li.innerHTML = `${input.value} <button onclick="this.parentNode.remove()">x</button>`
+
+    switch (prSelect.value) {
+      case 'High':
+        li.style.color = 'red';
+        break;
+      case 'Medium':
+        li.style.color = 'orange';
+        break;
+      case 'Low':
+        li.style.color = 'green';
+        break;
+      default:
+        null;
+        break;
+    }
+    ul.appendChild(li)
+    form.reset()
+  })
+ 
 });
+
