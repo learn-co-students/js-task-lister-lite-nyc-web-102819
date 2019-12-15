234c01bc08c86887a8489e8534f4bacdbe01ed3b
eliskim27 <eliskim27@gmail.com>
2019-12-14 19:06:37 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,48 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   // your code here
-// });
+document.addEventListener("DOMContentLoaded", () => {
+});
+//all constants
+const userInput = document.getElementById("new-task-description")
+const priority = document.getElementById('task-priority')
+const taskForm = document.getElementById("create-task-form")
+const tasksList = document.getElementById ("tasks")
+
+// creates new task
+  taskForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    // create new task li
+      let input = userInput.value
+      newLi = document.createElement('li')
+      newLi.innerText = input
+    // changes color based on priority
+      if (priority.value === "high"){
+        newLi.style.color = "#FF0000"}
+      else if (priority.value === "medium"){
+        newLi.style.color = "#FFD700"}
+      else if (priority.value === "low"){
+        newLi.style.color = "#008000"}
+        tasksList.append(newLi)
+    // create delete button
+      newDeleteButton = document.createElement('button')
+      newDeleteButton.innerText = "X"
+      newLi.appendChild(newDeleteButton)
+    // resets form
+      taskForm.reset()
+  })
+// deletes task
+  tasksList.addEventListener('click', function(e){
+    let targetLi = e.target.parentNode
+    targetLi.remove()
+  })
 
 
-// save use input from textbox
-function saveInput(){
-  const input = document.getElementById("new-task-description")
-  const newInput = input.value
-  return newInput
-}
-
-// create new task
-function createTask(input){
-  let li = document.createElement("li")
-  li.innerHTML = `
-  <h3> ${input} </h3>
-  `
-  return li
-}
-
-// append new task
-const taskList = document.getElementById("tasks")
-function appendTask(arg){
-  taskList.append(createTask(arg))
-}
-
-function run(event){
-  event.preventDefault()
-  appendTask(saveInput())
-}
-
-// submit button
-const form = document.getElementById("create-task-form")
 
 
-form.addEventListener("submit", run)
+
+
+// As a challenge, implement a sorting functionality that displays the tasks ASC/DESC based on priority
+
+// An additional input field (e.g. user, duration, date due)
+
+// Ability to edit tasks
+
+// Something of your choice! The main objective is to add a feature that allows the user's input to affect the DOM
